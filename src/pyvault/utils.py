5fb71ae57a94395e883f6a348b988d567fbaf9d4
Muhammed Ali Alp <malibaba2234@gmail.com>
2025-03-20 20:27:49 +0300

    fix progressbar message

--- src/pyvault/utils.py
+++ src/pyvault/utils.py
@@ -3,5 +3,5 @@ from .settings import MAX_FILE_CHAR_LEN, EMPTY_CHAR, FILL_CHAR
 
 
 def progressbar(filesize, filename):
-    filename = (filename if len(filename) < MAX_FILE_CHAR_LEN else filename[:MAX_FILE_CHAR_LEN] + '...') + ' '*((MAX_FILE_CHAR_LEN + 4) - len(filename))
+    filename = (filename if len(filename) < MAX_FILE_CHAR_LEN else filename[:MAX_FILE_CHAR_LEN] + '...') + ' '*((MAX_FILE_CHAR_LEN + 3) - len(filename))
     return click.progressbar(length=filesize, label=filename, bar_template=f"==> %(label)s [{click.style('%(bar)s', bg='blue')}] %(info)s", empty_char=EMPTY_CHAR, fill_char=FILL_CHAR)
