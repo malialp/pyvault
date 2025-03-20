284f724bbbf3c55ed8549ebcb1ce988c5852b505
Muhammed Ali Alp <malibaba2234@gmail.com>
2025-03-20 21:51:16 +0300

    change get config method

--- src/pyvault/decorators.py
+++ src/pyvault/decorators.py
@@ -3,6 +3,7 @@ import os
 import json
 
 from .settings import APP_VERSION
+from .vault import get_config
 
 def clear_console(command):
     @click.pass_context
@@ -25,20 +26,19 @@ def validate_vault(command):
             click.echo(click.style("Vault not initialized. Please run 'vault init' command to initialize the vault.", fg='red'))
             return
         else:
-            with open("config.json") as f:
-                config = json.load(f)
+            config = get_config()
 
-                major_version, minor_version, patch_version = APP_VERSION.split(".")
-                config_major_version, config_minor_version, config_patch_version = config.get("version").split(".") 
+            major_version, minor_version, patch_version = APP_VERSION.split(".")
+            config_major_version, config_minor_version, config_patch_version = config.get("version").split(".") 
 
-                if config.get("version") is None or config_major_version != major_version or config_minor_version != minor_version:
-                    click.echo(click.style("Invalid or outdated config.json found. Please update the app.", fg='red'))
-                    click.echo(click.style('Run "pip install --upgrade pyvault"', fg='yellow'))
-                    return
+            if config.get("version") is None or config_major_version != major_version or config_minor_version != minor_version:
+                click.echo(click.style("Invalid or outdated config.json found. Please update the app.", fg='red'))
+                click.echo(click.style('Run "pip install --upgrade pyvault"', fg='yellow'))
+                return
 
-                if config.get("salt") is None or config.get("salt") == "" or len(config.get("salt")) != 32:
-                    click.echo(click.style("Invalid salt found in config.json. Please reinitialize the vault.", fg='red'))
-                    return
+            if config.get("salt") is None or config.get("salt") == "" or len(config.get("salt")) != 32:
+                click.echo(click.style("Invalid salt found in config.json. Please reinitialize the vault.", fg='red'))
+                return
     
         return ctx.invoke(command, *args, **kwargs)
     
