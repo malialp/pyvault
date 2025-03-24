fcec4899898f0e70a9d51d9bdfa9f964ea1ec2a3
Muhammed Ali Alp <malibaba2234@gmail.com>
2025-03-24 19:22:37 +0300

    add random_string func

--- src/pyvault/utils.py
+++ src/pyvault/utils.py
@@ -1,7 +1,14 @@
 import click
+import random
+import string
+
 from .settings import MAX_FILE_CHAR_LEN, EMPTY_CHAR, FILL_CHAR
 
 
 def progressbar(filesize, filename):
     filename = (filename if len(filename) < MAX_FILE_CHAR_LEN else filename[:MAX_FILE_CHAR_LEN] + '...') + ' '*((MAX_FILE_CHAR_LEN + 3) - len(filename))
     return click.progressbar(length=filesize, label=filename, bar_template=f"==> %(label)s [{click.style('%(bar)s', bg='blue')}] %(info)s", empty_char=EMPTY_CHAR, fill_char=FILL_CHAR)
+
+
+def random_string(length):
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
